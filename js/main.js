07b1c169fd32c951c34ac5949ef55ed859d5985a
flopeace <florian.tchomga@gmail.com>
2022-11-20 19:42:48 +0100

    copy writing

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function loader_percentage() {
 
   let img = document.images,
     c = 0,
-    tot = img.length-24;
+    tot = img.length;
 
   for (let i = 0; i < tot; i++) {
     let tImg = new Image();
